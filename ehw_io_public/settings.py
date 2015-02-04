39667f0b4009baae0f7ded1582304b7291773bd8
Eric Williams <eric.williams@rackspace.co.uk>
2015-02-04 17:21:09 +0000

    Updating settings, installing packages

--- ehw_io_public/settings.py
+++ ehw_io_public/settings.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'markdown_deux',
+    'bootstrap3',
+    'xblog',
 )
 
 MIDDLEWARE_CLASSES = (
--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
-DJANGO_DB_ENGINE="django.db.backends.postgresql_psycopg2" \
-DJANGO_DB_NAME="db name" \
-DJANGO_DB_USER="db username goes here" \
-DJANGO_DB_PASSWORD="password goes here" \
-DJANGO_DB_HOST="db_ip_or_hostname" \
-DJANGO_DB_PORT="" \
-DJANGO_LANG="en-us" \
-DJANGO_TIMEZONE="UTC" \
-DJANGO_STATIC_URL="/static/" \
-    ./manage.py runserver 
+DJANGO_DB_ENGINE="django.db.backends.sqlite3" 
+DJANGO_DB_NAME="dbname.db" 
+DJANGO_DB_USER="db username goes here" 
+DJANGO_DB_PASSWORD="password goes here" 
+DJANGO_DB_HOST="db_ip_or_hostname" 
+DJANGO_DB_PORT="" 
+DJANGO_LANG="en-us" 
+DJANGO_TIMEZONE="UTC" 
+DJANGO_STATIC_URL="/static/" 
+
+export DJANGO_DB_ENGINE DJANGO_DB_NAME DJANGO_DB_USER DJANGO_DB_PASSWORD DJANGO_DB_HOST DJANGO_DB_PORT DJANGO_LANG DJANGO_TIMEZONE DJANGO_STATIC_URL
