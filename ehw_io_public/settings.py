0a71c3f9820bac2f5df3f67a3876dbe9fde6413b
Eric Williams <eric.williams@rackspace.co.uk>
2015-02-05 15:14:39 +0000

    Moved configuration to env

--- ehw_io_public/settings.py
+++ ehw_io_public/settings.py
@@ -98,5 +98,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
+TEMPLATE_DIRS=(os.environ.get("DJANGO_TEMPLATE_DIR"), os.path.join(BASE_DIR, "templates"),)
 STATIC_URL = os.environ.get("DJANGO_STATIC_URL",'/static/')
+SITE_ID = os.environ.get("DJANGO_SITE_ID")
