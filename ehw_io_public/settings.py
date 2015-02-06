675d36104d08d86e4b61ea0d07f7825624ba7c8b
Eric Williams <eric.williams@rackspace.co.uk>
2015-02-06 16:28:02 +0000

    Updated the settings with installed apps

--- ehw_io_public/settings.py
+++ ehw_io_public/settings.py
@@ -24,10 +24,21 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_HOSTNAMES", 'localhost 127.0.0.1').split()
 
 
 # Application definition
+
+CUMULUS = {
+    'USERNAME': os.environ.get("CUMULUS_USERNAME", "clouduser"),
+    'API_KEY': os.environ.get("CUMULUS_PASSWORD", "abcdefghk123123"),
+    'CONTAINER': os.environ.get('CUMULUS_CONTAINER', 'my-media-container')
+    'STATIC_CONTAINER': os.environ.get('CUMULUS_STATIC_CONTAINER', 'my-static-container'),
+    'PYRAX_IDENTITY_TYPE': 'rackspace',
+    'USE_PYRAX': True,
+    'USE_SSL': True,
+    'SERVICENET': True,
+}
 
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -37,9 +48,15 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django.contrib.flatpages',
+    'cumulus',
+    'haystack',
+    'django_xmlrpc',
+    'tinylinks',
     'markdown_deux',
     'bootstrap3',
     'xblog',
+    'social.apps.django_app.default',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -79,20 +96,13 @@ DATABASES = {
     }
 }
 
-print "--"
-print DATABASES
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
 LANGUAGE_CODE = os.environ.get("DJANGO_LANG", "en-us")
-
 TIME_ZONE = os.environ.get("DJANGO_TIMEZONE", "UTC")
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
@@ -101,3 +111,30 @@ USE_TZ = True
 TEMPLATE_DIRS=(os.environ.get("DJANGO_TEMPLATE_DIR"), os.path.join(BASE_DIR, "templates"),)
 STATIC_URL = os.environ.get("DJANGO_STATIC_URL",'/static/')
 SITE_ID = os.environ.get("DJANGO_SITE_ID")
+
+XMLRPC_METHODS = (
+    ('xblog.metaWeblog.blogger_deletePost', 'blogger.deletePost'),
+    ('xblog.metaWeblog.blogger_getRecentPosts', 'blogger.getRecentPosts'),
+    ('xblog.metaWeblog.blogger_getUserInfo', 'blogger.getUserInfo'),
+    ('xblog.metaWeblog.blogger_getUsersBlogs', 'blogger.getUsersBlogs'),
+    ('xblog.metaWeblog.wp_getUsersBlogs', 'wp.getUsersBlogs'),
+    ('xblog.metaWeblog.wp_getOptions', 'wp.getOptions'),
+    ('xblog.metaWeblog.metaWeblog_editPost', 'metaWeblog.editPost'),
+    ('xblog.metaWeblog.metaWeblog_getCategories', 'metaWeblog.getCategories'),
+    ('xblog.metaWeblog.metaWeblog_getPost', 'metaWeblog.getPost'),
+    ('xblog.metaWeblog.metaWeblog_getRecentPosts', 'metaWeblog.getRecentPosts'),
+    ('xblog.metaWeblog.metaWeblog_getUsersBlogs', 'metaWeblog.getUsersBlogs'),
+    ('xblog.metaWeblog.metaWeblog_newMediaObject', 'metaWeblog.newMediaObject'),
+    ('xblog.metaWeblog.metaWeblog_newPost', 'metaWeblog.newPost'),
+    ('xblog.metaWeblog.mt_getCategoryList', 'mt.getCategoryList'),
+    ('xblog.metaWeblog.mt_getPostCategories', 'mt.getPostCategories'),
+    ('xblog.metaWeblog.mt_publishPost', 'mt.publishPost'),
+    ('xblog.metaWeblog.mt_setPostCategories', 'mt.setPostCategories'),
+    ('xblog.metaWeblog.mt_supportedMethods', 'mt.supportedMethods'),
+    ('xblog.metaWeblog.mt_supportedTextFilters', 'mt.supportedTextFilters'),
+    ('xblog.metaWeblog.wp_getUsersBlogs', 'wp.getUsersBlogs'),
+    ('xblog.metaWeblog.wp_getOptions', 'wp.getOptions'),
+    ('xblog.metaWeblog.wp_getTags', 'wp.getTags'),
+)
+
+
