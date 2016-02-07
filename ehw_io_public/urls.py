735fecaf7866944d6a2a28429cbddcffc8d61872
Eric Williams <eric@subcritical.org>
2016-02-07 16:50:21 +0000

    Clean up urls, stop the tinylinks post-install crasher

--- ehw_io_public/urls.py
+++ ehw_io_public/urls.py
@@ -1,22 +1,23 @@
 from django.conf import settings 
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+import xblog.urls
+import social.apps.django_app.urls
+import haystack.urls
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ehw_io_public.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^accounts/', include('xblog.urls'), name='profile'),
+    url(r'^accounts/', include(xblog.urls), name='profile'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^blog/', include('xblog.urls'), name='blog'),
+    url(r'^blog/', include(xblog.urls), name='blog'),
     url(r'^xmlrpc/$', 'django_xmlrpc.views.handle_xmlrpc', name='xmlrpc'),
     url(r'^xmlrpc.php$', 'django_xmlrpc.views.handle_xmlrpc', name='xmlrpc'),
     url(r'^mt-xmlrpc.cgi$', 'django_xmlrpc.views.handle_xmlrpc', name='xmlrpc'),
-    url(r'^s/', include('tinylinks.urls')),
-    url('', include('social.apps.django_app.urls', namespace='social')),
-    url(r'^search/', include('haystack.urls'), name="search"),
+    url('', include(social.apps.django_app.urls, name='social')),
+    url(r'^search/', include(haystack.urls), name="search"),
     # url(r"^$", TemplateView.as_view(template_name='base.html')),
-    url(r'^$', include('xblog.urls')),
+    url(r'^$', include(xblog.urls)),
 
 )
 
