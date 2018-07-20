3dbe8ea5bb1120a9e595dd63015cdbe2523b845a
Eric Williams <ewilliams@ukcloud.com>
2018-07-20 16:36:38 +0100

    Health Check

--- ehw_io_public/urls.py
+++ ehw_io_public/urls.py
@@ -1,10 +1,12 @@
-from django.conf import settings
-from django.conf.urls import include, url
-from django.contrib import admin
 import xblog.urls
 import social.apps.django_app.urls
 import haystack.urls
+
+from django.conf import settings
+from django.conf.urls import include, url
+from django.contrib import admin
 from django_xmlrpc.views import handle_xmlrpc
+from .views import health
 
 urlpatterns = [
     # Examples:
@@ -12,6 +14,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^xmlrpc/$|^mt-xmlrpc.cgi$|^xmlrpc.php$', handle_xmlrpc, name='xmlrpc'),
     url(r'^search/', include(haystack.urls), name="search"),
+    url(r'health$', health),
     url('', include(social.apps.django_app.urls, namespace="social"), name='social'),
     url(r'', include(xblog.urls, namespace="xblog")),
 ]
